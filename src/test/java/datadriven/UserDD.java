1dce0a3f45d9275f4e73a33de70cb863271df873
Rbalbino <rafael.neco@hotmail.com>
2021-08-28 20:02:03 -0300

    Final da aula 2 - Leitura do arquivo users.csv como data provider do TestNG e utilizando para criar um Json Body parametrizado para inclusão de usuários

--- src/test/java/datadriven/UserDD.java
+++ src/test/java/datadriven/UserDD.java
@@ -1,32 +1,72 @@
 
 package datadriven;
+import org.json.JSONObject;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import utils.Data;
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 public class UserDD {
+    //3.1 Atributos
     String uri = "https://petstore.swagger.io/v2/user";
     Data data; // Objeto que representa a classe Utils.Data
 
     // 3.2 Métodos e Funçoes
+
+    @DataProvider // provedor de dados para os testes
+    public Iterator<Object[]> provider() throws IOException {
+       // List<String[]> testCases = new ArrayList<>();
+        List<Object[]> testCases = new ArrayList<>();
+        String[] testCase;
+        String linha;
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("db/users.csv"));
+        while((linha = bufferedReader.readLine()) !=null) {
+            testCase = linha.split(",");
+            testCases.add(testCase);
+        }
+
+        return testCases.iterator();
+    }
 
     @BeforeMethod // antes do método de teste
     public void setup (){
         data = new Data();
     }
 
-    @Test
-    public void incluirUsuario() throws IOException {
+    @Test (dataProvider = "provider") // Método de teste
+    public void incluirUsuario(
+            String id,
+            String username,
+            String firstName,
+            String lastName,
+            String email,
+            String password,
+            String phone,
+            String userStatus) throws IOException {
 
-        String jsonBody = data.lerJson("db/user1.json");
+        String jsonBody = new JSONObject()
+               .put("id", id)
+               .put("username", username)
+               .put("firstName", firstName)
+               .put("lastName", lastName)
+               .put("email", email)
+               .put("password", password)
+               .put("phone", phone)
+               .put("userStatus", userStatus)
+               .toString();
 
         String userId =
                 given()
